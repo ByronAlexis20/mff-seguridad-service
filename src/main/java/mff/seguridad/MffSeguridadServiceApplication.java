5befbe055afaa2ddeca7f6f6381ac39dacd3e013
ByronAlexis20 <byronalexisramirez20@gmail.com>
2022-06-07 18:05:10 -0500

    start project

---
+++ src/main/java/mff/seguridad/MffSeguridadServiceApplication.java
@@ -0,0 +1,13 @@
+package mff.seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MffSeguridadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MffSeguridadServiceApplication.class, args);
+	}
+
+}
